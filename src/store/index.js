0d822ceb37670c5bb9ce60434412f8a495783759
Stan <bernardstanislas@gmail.com>
2015-11-18 22:47:15 +0100

    [redux] Add sample store

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import {createStore, compose, applyMiddleware} from 'redux';
+import createNodeLogger from 'redux-node-logger';
+import app from '../reducers';
+
+const loggerMiddleware = createNodeLogger({});
+
+const createStoreWithMiddleware = compose(
+    applyMiddleware(
+        // loggerMiddleware
+    )
+)(createStore);
+
+const store = createStoreWithMiddleware(app);
+
+
+const unsubscribe = store.subscribe(() => {
+    console.log('Store changed !');
+});
+
+export default store;
