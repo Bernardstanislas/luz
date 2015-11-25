b82eb4bcfdbf40d81758165954edde5d1f78721d
Stan <bernardstanislas@gmail.com>
2015-11-25 01:05:12 +0100

    [actions] Add triggers and updaters

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,19 @@
-export const LOG_EVENT = 'LOG_EVENT';
+export const UPDATE_TRIGGERS = 'UPDATE_TRIGGERS';
+export const SWITCH_RELAY = 'SWITCH_RELAY';
 
-export const logEvent = (event) => {
+export const updateTriggers = (timesheet, relayId) => {
     return ({
-        type: LOG_EVENT,
-        event
+        type: UPDATE_TRIGGERS,
+        timesheet,
+        relayId
+    });
+}
+
+export const switchRelay = (relayId, switched, manual = false) => {
+    return ({
+        type: SWITCH_RELAY,
+        relayId,
+        switched,
+        manual
     });
 }
