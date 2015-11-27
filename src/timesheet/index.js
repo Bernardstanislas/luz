7f7cb758cc617caaa4b096ec2797e8b60ce92459
Stan <bernardstanislas@gmail.com>
2015-11-27 20:54:46 +0100

    Refactor timesheet reducing

---
+++ src/timesheet/index.js
@@ -0,0 +1,15 @@
+import {CronJob} from 'cron';
+import store from '../store';
+import {scheduledSwitchRelay} from '../actions';
+
+const timeEntryToTrigger = ({h, md, d}, action) => new CronJob(`00 ${m} ${h} * * ${d}`, action, null, false);
+const buildSwitchActionDispatch = (relayId, switched) => store.dispatch(scheduledSwitchRelay(relayId, switched));
+
+const timesheetEntryToTriggers = ({from, to}, relayId) => [
+    timeEntryToTrigger(from, buildSwitchActionDispatch(relayId, true),
+    timeEntryToTrigger(to, buildSwitchActionDispatch(relayId, false),
+];
+
+const filterActiveTimesheetEntries = ({active}) => active;
+
+export const timesheetEntriesToTriggers = (timesheet, relayId) => timesheet.filter(filterActiveTimesheetEntries).map(entry => timesheetEntryToTriggers(entry, relayId));
