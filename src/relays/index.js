54958249abb75aa819859fd5992d72098de7693a
Stanislas Bernard <bernardstanislas@gmail.com>
2015-12-05 13:58:45 +0100

    [relay] Use ready callback

--- src/relays/index.js
+++ src/relays/index.js
@@ -7,11 +7,12 @@ const relaysRef = firebase.child('relays');
 const hostname = os.hostname();
 if (hostname !== 'raspberrypi') console.log(`You are running the application on something else than a raspberry pi (${hostname}). Switches will be faked.`);
 
-const gpio2 = gpio.export(2, {direction: 'out'});
-const gpio3 = gpio.export(3, {direction: 'out'});
-
-relaysRef.child('relay1').child('switched').once('value', snapshot => gpio2.set(snapshot.val() ? 0 : 1));
-relaysRef.child('relay2').child('switched').once('value', snapshot => gpio3.set(snapshot.val() ? 0 : 1));
+const gpio2 = gpio.export(2, {direction: 'out', ready() {
+    relaysRef.child('relay1').child('switched').once('value', snapshot => gpio2.set(snapshot.val() ? 0 : 1));
+}});
+const gpio3 = gpio.export(3, {direction: 'out', ready() {
+    relaysRef.child('relay2').child('switched').once('value', snapshot => gpio3.set(snapshot.val() ? 0 : 1));
+}});
 
 gpio2.on('change', val => {
    relaysRef.child('relay1').child('switched').set(val === 0);
