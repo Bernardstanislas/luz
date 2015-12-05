6579eabb257761ce32c93e5fb544ca2264382773
Stanislas Bernard <bernardstanislas@gmail.com>
2015-12-05 13:17:57 +0100

    [relay] Change switches

--- src/relays/index.js
+++ src/relays/index.js
@@ -7,21 +7,21 @@ const relaysRef = firebase.child('relays');
 const hostname = os.hostname();
 if (hostname !== 'raspberrypi') console.log(`You are running the application on something else than a raspberry pi (${hostname}). Switches will be faked.`);
 
-const gpio2 = gpio.export(2, {direction: 'out'}, () => {gpio2.set(0)});
-const gpio3 = gpio.export(3, {direction: 'out'}, () => {gpio3.set(0)});
+const gpio2 = gpio.export(2, {direction: 'out'}, () => {gpio2.set(1)});
+const gpio3 = gpio.export(3, {direction: 'out'}, () => {gpio3.set(1)});
 
 gpio2.on('change', val => {
-   relaysRef.child('relay1').child('switched').set(val === 1);
+   relaysRef.child('relay1').child('switched').set(val === 0);
    relaysRef.child('relay1').child('manual').set(false);
 });
 gpio3.on('change', val => {
-   relaysRef.child('relay2').child('switched').set(val === 1);
+   relaysRef.child('relay2').child('switched').set(val === 0);
    relaysRef.child('relay2').child('manual').set(false);
 });
 
 const switchGpio = (port, switched) => {
     if (hostname === 'raspberrypi') {
-        port.set(switched ? 1 : 0);
+        port.set(switched ? 0 : 1);
     }
     console.log(`Port ${port.headerNum} is now switched ${switched ? 'on' : 'off'}.`);
 }
