3e0654f3fcf86af58226497419dd848e927f6c68
Stanislas Bernard <bernardstanislas@gmail.com>
2015-12-05 13:31:25 +0100

    [relay] Fix initial value

--- src/relays/index.js
+++ src/relays/index.js
@@ -7,8 +7,12 @@ const relaysRef = firebase.child('relays');
 const hostname = os.hostname();
 if (hostname !== 'raspberrypi') console.log(`You are running the application on something else than a raspberry pi (${hostname}). Switches will be faked.`);
 
-const gpio2 = gpio.export(2, {direction: 'out'}, () => {gpio2.set(1)});
-const gpio3 = gpio.export(3, {direction: 'out'}, () => {gpio3.set(1)});
+const gpio2 = gpio.export(2, {direction: 'out'}, () => {
+    relaysRef.child('relay1').child('switched').once('value', snapshot => gpio2.set(snapshot.val() ? 0 : 1));
+});
+const gpio3 = gpio.export(3, {direction: 'out'}, () => {
+    relaysRef.child('relay2').child('switched').once('value', snapshot => gpio3.set(snapshot.val() ? 0 : 1));
+});
 
 gpio2.on('change', val => {
    relaysRef.child('relay1').child('switched').set(val === 0);
