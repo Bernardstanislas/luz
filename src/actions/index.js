d68294f209b121d43ab7270374dfad2cd4b6fdae
Stanislas Bernard <bernardstanislas@gmail.com>
2016-02-07 19:16:40 +0100

    Fix init

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,26 +2,20 @@ export const UPDATE_TRIGGERS = 'UPDATE_TRIGGERS';
 export const SCHEDULED_SWITCH_RELAY = 'SCHEDULED_SWITCH_RELAY';
 export const MANUAL_SWITCH_RELAY = 'MANUAL_SWITCH_RELAY';
 
-export const updateTriggers = (timesheet, relayId) => {
-    return ({
-        type: UPDATE_TRIGGERS,
-        timesheet,
-        relayId
-    });
-}
+export const updateTriggers = (timesheet, relayId) => ({
+    type: UPDATE_TRIGGERS,
+    timesheet,
+    relayId
+});
 
-export const scheduledSwitchRelay = (relayId, switched) => {
-    return ({
-        type: SCHEDULED_SWITCH_RELAY,
-        relayId,
-        switched
-    });
-}
+export const scheduledSwitchRelay = (relayId, switched) => ({
+    type: SCHEDULED_SWITCH_RELAY,
+    relayId,
+    switched
+});
 
-export const manualSwitchRelay = (relayId, switched) => {
-    return ({
-        type: MANUAL_SWITCH_RELAY,
-        relayId,
-        switched
-    });
-}
+export const manualSwitchRelay = (relayId, switched) => ({
+    type: MANUAL_SWITCH_RELAY,
+    relayId,
+    switched
+});
--- src/relays/index.js
+++ src/relays/index.js
@@ -14,37 +14,23 @@ const gpio3 = gpio.export(3, {direction: 'out', ready() {
     relaysRef.child('relay2').child('switched').once('value', snapshot => gpio3.set(snapshot.val() ? 0 : 1));
 }});
 
-gpio2.on('change', val => {
-   relaysRef.child('relay1').child('switched').set(val === 0);
-   relaysRef.child('relay1').child('manual').set(false);
-});
-gpio3.on('change', val => {
-   relaysRef.child('relay2').child('switched').set(val === 0);
-   relaysRef.child('relay2').child('manual').set(false);
-});
-
-const switchGpio = (port, switched) => {
+const switchGpio = (port, switched, relay) => {
     if (hostname === 'raspberrypi') {
         port.set(switched ? 0 : 1);
+    } else {
+        console.log(`Port ${port.headerNum} is now switched ${switched ? 'on' : 'off'}.`);
     }
-    console.log(`Port ${port.headerNum} is now switched ${switched ? 'on' : 'off'}.`);
+    relaysRef.child(relay).child('switched').set(switched);
+    relaysRef.child(relay).child('manual').set(false);
 }
 
 export const switchRelay = (relayId, switched) => {
     switch (relayId) {
         case 'relay1':
-            switchGpio(gpio2, switched);
-            if (hostname !== 'raspberrypi') {
-                relaysRef.child('relay1').child('switched').set(switched);
-                relaysRef.child('relay1').child('manual').set(false);
-            }
+            switchGpio(gpio2, switched, 'relay1');
             break;
         case 'relay2':
-            switchGpio(gpio3, switched);
-            if (hostname !== 'raspberrypi') {
-                relaysRef.child('relay2').child('switched').set(switched);
-                relaysRef.child('relay2').child('manual').set(false);
-            }
+            switchGpio(gpio3, switched, 'relay2');
             break;
     }
 }
